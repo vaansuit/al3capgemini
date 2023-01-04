69da988d98c0a1320515c2267373e603cc74761b
unknown <vvansuit@outlook.com>
2023-01-04 15:26:45 -0300

    added content to main in VariaveisTitulosOperadores

--- VariaveisTiposOperadores/src/Main.java
+++ VariaveisTiposOperadores/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int idade = 19;
+        double cotacaoDolar = 5.90d;
+        float cotacaoEuro = 7.90f;
+        char genero = 'M';
+        byte ponto = 0;
+        boolean boleano = false;
+        String nome = "Meu Nome";
         
     }
 }
