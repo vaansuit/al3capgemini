cb4e0e04c9ca76c57ef4de2b4dcd0f171b99e4b8
unknown <vvansuit@outlook.com>
2023-01-05 23:18:23 -0300

    for in java

---
+++ ForStructure/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int numero = 10;
+
+        for (; numero > 10; numero = numero + 1) {
+            System.out.println(numero);
+        }
+    }
+}
